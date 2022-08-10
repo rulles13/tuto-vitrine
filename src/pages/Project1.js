9830d5fd9be87123c8f8f9df75f9d46ba9650018
Stephane Gillon <stephanegillon@hotmail.com>
2022-08-10 09:47:00 +0200

    navigation + page 404 ok

---
+++ src/pages/Project1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Project1 = () => {
+    return (
+        <div>
+            Projet 1
+        </div>
+    );
+};
+
+export default Project1;
---
+++ src/pages/Project4.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Project4 = () => {
+    return (
+        <div>
+            Projet 4
+        </div>
+    );
+};
+
+export default Project4;
