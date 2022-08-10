6cb80342ebf288fcd0d4117208d7afb6f32c3d66
Stephane Gillon <stephanegillon@hotmail.com>
2022-08-10 15:02:28 +0200

    dynamic text ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import DynamicText from '../components/DynamicText';
+import Mouse from '../components/Mouse';
+import Navigation from '../components/Navigation';
+import SocialNetwork from '../components/SocialNetwork';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Mouse />
+            <div className="home">
+                <Navigation />
+                <SocialNetwork />
+                <div className="home-main">
+                    <div className="main-content">
+                        <h1>FS AGENCY</h1>
+                        <h2><DynamicText /></h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
